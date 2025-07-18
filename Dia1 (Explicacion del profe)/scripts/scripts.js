69f6d3fd1c6cb0da9b88a8235487970475fe6321
robinsondominguez <robinsondominguezulloa349@gmail.com>
2025-07-18 12:16:17 -0500

    feat: :sparkles: First Dia1 JavaScripts

---
+++ Dia1 (Explicacion del profe)/scripts/scripts.js
@@ -0,0 +1,41 @@
+//############
+//### Dia1 ###
+//############
+
+//Toco primer comando UnU
+console.log("Hola Mundo!");
+variable1= 3.12;
+
+//Numeros
+console.log(variable1);
+console.log(typeof variable1)
+
+//Concatenacion
+palabra1="Campus";
+palabra2="lands";
+PalabraFinal=palabra1+palabra2
+console.log(PalabraFinal)
+
+//Booleano
+booleanito1=true;
+booleanito=false;
+console.log(typeof booleanito1);
+
+//Condicionales
+if(booleanito == true){
+    console.log("Soy Verdadero UwU!");
+}else{
+    console.log("No soy Verdadero UnU");
+}
+
+//Bucles
+for(i=0;i<4;i=i+1){
+    console.log("Wenas");
+} 
+
+holi = true;
+while(holi==true){
+    console.log("Soy Verdadero UwU!");
+}
+
+// Desarrollado por: Dominguezrobinson - T.I:1.104.128.994
---
+++ Dia1/scripts/scripts.js
@@ -0,0 +1,8 @@
+//############
+//### Dia1 ###
+//############
+
+//Toco primer comando UnU
+console.log("Hola Mundo!");
+
+// Desarrollado por: DominguezRobinson - T.I:1.104.128.994
