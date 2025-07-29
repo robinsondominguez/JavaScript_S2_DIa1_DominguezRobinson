b446d6e29371a70f469e67e5221042b6c9f1a40b
robinsondominguez <robinsondominguezulloa349@gmail.com>
2025-07-29 12:46:35 -0500

    feat: :sparkles: Activity Dia 8

---
+++ Dia 8 (Actividad)/script/script.js
@@ -0,0 +1,21 @@
+//function buscarPersonaje() {
+    const nombre = prompt("Character Name:")
+    const xhr = new XMLHttpRequest();
+    const url = `https://rickandmortyapi.com/api/character?name=${nombre}`;
+    console.log(url);
+    xhr.open("GET",url,true);
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState && xhr.responseText) {
+            try{
+                const daticos = JSON.parse(xhr.responseText)
+                alert("The character is:\n Name: "+ daticos["results"][0]["name"]+" \n Status: "+daticos["results"][0]["status"]+ "\n  Specie: "+daticos["results"][0]["species"]);
+            }
+            catch(err){
+                console.log(err.message);
+            }
+        }
+    };
+    xhr.send();
+}
+
+//buscarPersonaje()
