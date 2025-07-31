6fbd9a12e15c554248246b6851a9d1919f224032
robinsondominguez <robinsondominguezulloa349@gmail.com>
2025-07-31 12:22:47 -0500

    feat: :hammer: js code adding

--- Dia 9 (Pokedes)/scripts/script.js
+++ Dia 9 (Pokedes)/scripts/script.js
@@ -1,2 +1,17 @@
-const xhr = new XMLHttpRequest();
-const URL =  ``
+const namePokemon = document.getElementById('pokemonName');
+const idPokemon = document.getElementById('pokemonID');
+const imagePokemon = document.getElementById('imagePokemon');
+
+const form = document.getElementById('Searchs');
+const input = document.getElementById('search');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+
+let fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (APIResponse.status == 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
+}
