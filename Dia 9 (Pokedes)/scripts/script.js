933f6fd44c52654dd3532f1713cb81f87f5ed1fd
robinsondominguez <robinsondominguezulloa349@gmail.com>
2025-07-31 17:28:57 -0500

    feat: :hammer: :)

--- Dia 9 (Pokedes)/scripts/script.js
+++ Dia 9 (Pokedes)/scripts/script.js
@@ -1,13 +1,15 @@
-const namePokemon = document.getElementById('pokemonName');
-const idPokemon = document.getElementById('pokemonID');
-const imagePokemon = document.getElementById('imagePokemon');
+const pokemonName = document.getElementById('pokemonName');
+const pokemonNumber = document.getElementById('pokemonID');
+const pokemonImage = document.getElementById('imagePokemon');
 
 const form = document.getElementById('Searchs');
 const input = document.getElementById('search');
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
+const buttonPrev = document.getElementById('prev');
+const buttonNext = document.getElementById('next');
 
-let fetchPokemon = async (pokemon) => {
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
     if (APIResponse.status == 200) {
@@ -17,7 +19,41 @@ let fetchPokemon = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon) => {
-    namePokemon.
-    idPokemon
-    imagePokemon
+    pokemonImage.style.display = 'none'
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['front_shiny'];
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none'
+        pokemonName.innerHTML = "No.";
+        pokemonNumber.innerHTML = '';
+    }
+
 }
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+})
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
