1b64c2bc61fbd69b886933d9b156b3adb5bd15e0
robinsondominguez <robinsondominguezulloa349@gmail.com>
2025-08-05 19:36:36 -0500

    feat: :sparkles: Mondongo

--- Dia12+1/script/script.js
+++ Dia12+1/script/script.js
@@ -1,24 +1,42 @@
+const token = 'a11cb1eb4d022a8a451968d8f832476c'
+
 function buscarPersonaje() {
-    const inputSearch = document.getElementById('inputSearch');
-
-    const xhr = new XMLHttpRequest();
-    const url = `https://superheroapi.com/api/a11cb1eb4d022a8a451968d8f832476c/${Heroes_Name}`;
-
-    console.log(url);
-
-    xhr.open("GET",url,true);
+    document.getElementById("resultados").innerHTML=``;
+    //const nombreUsar= prompt("Ingresar el nombre del personaje a buscar:");
+    const nombreUsar = document.getElementById("nombreInput").value.trim();
+    const xhr = new XMLHttpRequest();//La importaciÃ³n del XML
+    const url = `https://superheroapi.com/api.php/${token}/search/${nombreUsar}`;//Acento Inverso ```
+    xhr.open("GET", url, true);
+    
     xhr.onreadystatechange = function () {
-        if (xhr.readyState == 3) {
-            console.log('...')
-        }else if (xhr.readyState == 4 && xhr.status == 200) {
-            
+        if (xhr.readyState === 3) {
+
+            console.log("Cargando...");
         }
-    }
+        else if (xhr.readyState === 4 && xhr.status === 200) {
+            try {
+               
+                const daticos = JSON.parse(xhr.responseText);
+                if (daticos.results && daticos.results.length > 0) {
+                    for (let i = 0; i < daticos.results.length; i++) {
+                        let division = document.getElementById("resultados");
+                        division.innerHTML += `
+                        <div class="card">
+            <img src="${daticos["results"][i]["image"]["url"]}" alt="">
+            <h3>${daticos["results"][i]["name"]}</h3>
+            <p><strong>NombreCompleto: </strong>${daticos["results"][i]["biography"]["full-name"]}</p>
+            <p><strong>Specie:</strong>${daticos["results"][i]["species"]}</p>
+        </div>
+                        `
+                        console.log(daticos["results"][i]["name"]);
+                    }
+                }
 
-
-
+            }
+            catch (err) {
+                console.log(err.message); //Si algo malo pasa simplemente imprima el error
+            }
+        }
+    };
+    xhr.send();
 }
-
-
-
-
